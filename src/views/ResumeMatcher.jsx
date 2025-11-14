288c5bce09b9e43cfc5e639a30dda1aff0de1ecd
jjlindsey <jlindsey010@gmail.com>
2025-11-14 09:02:44 -0800

    create view

---
+++ src/views/ResumeMatcher.jsx
@@ -0,0 +1,31 @@
+import ExtractorUI from "../components/ExtractorUI"
+import { extractResumeFields } from "../utils/heuristics"
+
+export default function ResumeMatcher() {
+    const [jobDescription, setJobDescription] = useState('')
+    const [resumeText, setResumeText] = useState('')
+    const [results, setResults] = useState(null)
+    //const [loading, setLoading] = useState(false)
+
+    const handleExtract = () => {
+        const jdKeywords = jobDescription
+            .toLowerCase()
+            .split(/\W+/)
+            .filter(w => w.length > 2)
+        
+        const parsed = extractResumeFields(resumeText, jdKeywords)
+        setMatchingResults(parsed)
+    }
+    return (
+        <>
+            <ExtractorUI 
+                resumeText={resumeText}
+                setResumeText={setResumeText}
+                onExtract={handleExtract}
+            />
+            {results && (
+                <ResultViewer results={results} onBack={() => setResults(null)} />
+            )}
+        </>
+    )
+}
