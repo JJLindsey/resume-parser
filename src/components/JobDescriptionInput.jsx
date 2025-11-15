1d51ac08548baf67fa4a2ab9bded5d11ef1d10dd
jjlindsey <jlindsey010@gmail.com>
2025-11-14 16:20:40 -0800

    update job descr file

--- src/components/JobDescriptionInput.jsx
+++ src/components/JobDescriptionInput.jsx
@@ -1,24 +1,10 @@
 import { useState } from "react"
 import { Paper, TextField, Button, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
 
-export default function JobDescriptionInput({value, onChange, onExtract}) {
-    const [mode, setMode] = useState('hybrid')
+export default function JobDescriptionInput({value, onChange, onExtract, disabled}) {
+    //const [mode, setMode] = useState('hybrid')
     return (
-        <>
             <Paper sx={{ p: 3, mb: 4 }}>
-                <FormControl fullWidth sx={{ mb: 2 }}>
-                <InputLabel id="mode-select-label">Analysis Mode</InputLabel>
-                    <Select
-                        labelId="mode-select-label"
-                        value={mode}
-                        label="Analysis Mode"
-                        onChange={(e) => setMode(e.target.value)}
-                    >
-                        <MenuItem value="jd">Job Description Only</MenuItem>
-                        <MenuItem value="lexicon">Skills Lexicon Only</MenuItem>
-                        <MenuItem value="hybrid">Hybrid</MenuItem>
-                    </Select>
-                </FormControl>
                 <TextField
                     multiline
                     minRows={5}
@@ -26,11 +12,11 @@ export default function JobDescriptionInput({value, onChange, onExtract}) {
                     placeholder="Paste job description here..."
                     value={value}
                     onChange={e => onChange(e.target.value)}
+                    disabled={disabled}
                 />
-                 <Button variant='contained' onClick={onExtract} disabled={!value}>
+                 <Button variant='contained' onClick={onExtract} disabled={!value || disabled} sx={{ mt: 2 }}>
                     Analyze Job Description
                 </Button>
             </Paper>
-        </>
     )
 }
